11708fa6552b20b8db9489aba03294ee76c0c2ff
toporras <to.porras@duocuc.cl>
2026-09-08 14:03:47 -0300

    implementacion de script de base de datos con los productos

--- js/admin.js
+++ js/admin.js
@@ -102,7 +102,7 @@ async function guardarNuevoProducto(e) {
     alert("¡Producto agregado localmente!");
   }
 
-  // Cerrar Modal y resetear formulario
+  // resetear formulario
   const modalEl = document.getElementById("modalCrearProducto");
   const modal = bootstrap.Modal.getInstance(modalEl);
   modal.hide();
@@ -116,7 +116,7 @@ function eliminarProducto(id) {
   }
 }
 
-// ================= 2. MANTENEDOR DE USUARIOS =================
+// ususarios
 
 function renderizarTablaUsuarios() {
   const tbody = document.getElementById("tabla-usuarios-body");
@@ -156,7 +156,7 @@ function guardarNuevoUsuario(e) {
   renderizarTablaUsuarios();
   alert("¡Usuario creado exitosamente!");
 
-  // Cerrar Modal y resetear formulario
+  //  resetear formulario
   const modalEl = document.getElementById("modalCrearUsuario");
   const modal = bootstrap.Modal.getInstance(modalEl);
   modal.hide();
